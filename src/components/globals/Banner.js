7c126181a7d7ff2a93f74ede003dfea4e3514efc
onTheDL <daniel.laserna@protonmail.com>
2020-12-07 00:03:07 -0500

    Add header animation

--- src/components/globals/Banner.js
+++ src/components/globals/Banner.js
@@ -1,21 +1,83 @@
-import React from 'react'
-import styled, { css, keyframes } from 'styled-components'
-import { setColor } from '../../styles'
+import React from "react";
+import styled, { css, keyframes } from "styled-components";
+import {
+  setColor,
+  setRem,
+  setLetterSpacing,
+  setBorder,
+  media,
+} from "../../styles";
+
+const fadeIn = (start, mid, end) => {
+  const animation = keyframes`
+    0%{
+      opacity: 0;
+      transform: translateY(${start})
+    }
+    50%{
+      opacity: 0.5;
+      transform: translateY(${mid})
+    }
+    100%{
+      opacity: 1;
+      transform: translateY(${end})
+    }
+  `;
+  return css`
+    animation: ${animation} 3s ease-in-out;
+  `;
+};
 
 function Banner({ className, title, text, children, greeting }) {
   return (
     <div className={className}>
-      <h1>{greeting} <span>{title}</span></h1>
-      <div className='info'>
+      <h1>
+        {greeting} <span>{title}</span>
+      </h1>
+      <div className="info">
         <p>{text}</p>
         {children}
       </div>
-    </div>   
-  )
+    </div>
+  );
 }
 
 const BannerWrapper = styled(Banner)`
-  color: blue;
-`
+  background: rgba(0, 0, 0, 0.7);
+  color: ${setColor.mainWhite};
+  text-align: center;
+  padding: ${setRem(200)} ${setRem(32)};
+  ${setLetterSpacing(3)};
+  h1 {
+    text-transform: capitalize;
+    font-size: ${setRem(48)};
+    color: ${setColor.primaryColor};
+    span {
+      color: ${setColor.mainWhite};
+    }
+  }
+  p {
+    width: 85%;
+    margin: 0 auto;
+  }
 
-export default BannerWrapper
+  ${media.tablet`
+    width: 70vw;
+    ${setBorder({ width: "6px", color: setColor.primaryColor })};
+    p {
+      width: 75%;
+    }
+  `}
+
+  h1 {
+    /* animation */
+    ${fadeIn('100%', '-10%', '0')}
+  }
+  .info {
+    /* animation */
+    ${fadeIn('-100%', '10%', '0')}
+
+  }
+`;
+
+export default BannerWrapper;
--- src/styles.js
+++ src/styles.js
@@ -1,3 +1,5 @@
+import { css } from "styled-components";
+
 export const setColor = {
   primaryColor: "#af9a7d",
   mainWhite: "#fff",
@@ -28,3 +30,38 @@ export const setBackground = ({
       url(${img}) center/cover fixed no-repeat;
   `;
 };
+
+export const setRem = (number = 16) => {
+  return `
+    ${number / 16}rem
+  `;
+};
+
+export const setLetterSpacing = (number = 2) => {
+  return `letter-spacing: ${number}px`;
+};
+
+export const setBorder = ({
+  width = "2px",
+  style = "solid",
+  color = "black",
+} = {}) => {
+  return `border: ${width} ${style} ${color}`;
+};
+
+const sizes = {
+  large: 1200,
+  desktop: 992,
+  tablet: 768,
+  phone: 576,
+};
+
+//Iterate through the sizes and create a media template
+export const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (min-width: ${sizes[label] / 16}em) {
+      ${css(...args)}
+    }
+  `;
+  return acc;
+}, {});
