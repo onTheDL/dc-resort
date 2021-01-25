0b4ce20ddf2fd058027168c0cee45909c04cf40e
Daniel Laserna <daniel.laserna@protonmail.com>
2021-01-25 14:52:22 -0500

    Production build

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Hero from "../globals/Hero";
-import aboutImg from "../../images/aboutBcg.jpeg";
 import homeImg from "../../images/homeBcg.jpeg";
 import Banner from "../globals/Banner";
 import { PrimaryBtn } from '../globals/Buttons';
@@ -11,7 +10,7 @@ export default function Header() {
       <Banner
         greeting="welcome to"
         title="dreamers cove resort"
-        text="I'm baby etsy blog distillery cliche aesthetic. Scenester selvage hell of 3 wolf moon taxidermy marfa coloring book. Kale chips woke iPhone leggings chia blog quinoa lomo."
+        text="I'm baby etsy blog distillery cliche aesthetic. Scenester selvage avocado of 3 wolf moon taxidermy marfa coloring book. Kale chips woke iPhone leggings chia blog quinoa lomo."
       >
         <PrimaryBtn t='2.5rem'>
           View Details
